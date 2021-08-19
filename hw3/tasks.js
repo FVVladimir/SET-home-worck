df8d0ecf976692e929b3abb2a6a98fd2ebd606e2
FVVladimir <55664838+FVVladimir@users.noreply.github.com>
2021-08-19 22:24:56 +0300

    первая задача к 3 дз нагугленная

--- hw3/tasks.js
+++ hw3/tasks.js
@@ -1,8 +1,21 @@
 // ===========================================task1========================================================
 
 let s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
-let ss = s.toUpperCase();
-console.log(ss.split(';'));
+
+// ==================================================-1-===============================================================
+console.log(s.split(';')
+    .map(n => n.split(':').reverse().join(':'))
+    .sort()
+    .map(n => `(${n.split(':')[0]}, ${n.split(':')[1]})`.toUpperCase())
+    .join(``));
+
+
+// ====================================================-2-=======================================================
+// const s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+//   return s.toUpperCase().split(';')
+//           .map(name => name.split(':').reverse())
+//           .sort()
+//           .reduce((accumulator, name) => ( accumulator += `(${name[0]}, ${name[1]})`), "");
 
 // =============================================task2==========================================================
 
@@ -15,18 +28,19 @@ let chairs = [
 
 const meeting = (arr, need) => {
     let freeChair = [];
-
+    let result = 0;
     for (i = 0; i < arr.length; i += 1) {
         freeChair.push(arr[i][1] - arr[i][0].length);
         console.log(freeChair);
-
+        result = freeChair.reduce(function (sum, elem) {
+            return sum + elem;
+        }, 0);
+        if (result >= need) break;
     }
-    let result = freeChair.reduce(function (sum, elem) {
-        return sum + elem;
-    }, 0);
+
     console.log(result);
 };
 
-meeting(chairs);
+meeting(chairs, 7);
 
 
